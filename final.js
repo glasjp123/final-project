555ee1b6bebf358dc51b8ef33fd5e106818350cf
glasjp123 <jwilki02@gmail.com>
2018-11-12 01:34:54 -0500

    Adds game rules and tallies results

--- final.js
+++ final.js
@@ -4,27 +4,58 @@ function generateComputerNum() {
   var random = computer[Math.floor(Math.random()*computer.length)];
     if (random === 1){
     computer = "Rock!";
-    }else if (random === 2){
+    } else if (random === 2){
     computer = "Paper!";
-    }else {
-    computer = "Scissor!";
+    } else {
+    computer = "Scissors!";
     }
     document.getElementById('computer-display').value = computer;
 }
 
+function gameRules() {
+    var user = document.getElementById('user-display').value,
+        computer = document.getElementById('computer-display').value,
+        wins = parseInt(document.getElementById('win').value),
+        losses = parseInt(document.getElementById('loss').value),
+        ties = parseInt(document.getElementById('tie').value);
+
+    if (user==="Rock!" && computer==="Rock!"){
+    document.getElementById('tie').value=(ties=ties+1);
+    } else if (user==="Rock!" && computer==="Paper!"){
+    document.getElementById('loss').value=(losses=losses+1);
+    } else if (user==="Rock!" && computer==="Scissors!"){
+    document.getElementById('win').value=(wins=wins+1);
+    } else if (user==="Paper!" && computer==="Paper!"){
+    document.getElementById('tie').value=(ties=ties+1);
+    } else if (user==="Paper!" && computer==="Rock!"){
+    document.getElementById('win').value=(wins=wins+1);
+    } else if (user==="Paper!" && computer==="Scissors!"){
+    document.getElementById('loss').value=(losses=losses+1);
+    } else if (user==="Scissors!" && computer==="Scissors!"){
+    document.getElementById('tie').value=(ties=ties+1);
+    } else if (user==="Scissors!" && computer==="Paper!"){
+    document.getElementById('win').value=(wins=wins+1);
+    } else if (user==="Scissors!" && computer==="Rock!"){
+    document.getElementById('loss').value=(losses=losses+1);
+    }
+}
 
 function rockSelect() {
     document.getElementById('user-display').value = "Rock!";
     generateComputerNum();
+    gameRules();
 }
 function paperSelect() {
     document.getElementById('user-display').value = "Paper!";
     generateComputerNum();
+    gameRules();
 }
 function scissorsSelect() {
     document.getElementById('user-display').value = "Scissors!";
     generateComputerNum();
+    gameRules();
 }
+
 
 document.getElementById('rock-selector').onclick=rockSelect;
 document.getElementById('paper-selector').onclick=paperSelect;
