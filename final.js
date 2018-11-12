9121028694f42b59f1c817159f896fc076933089
glasjp123 <jwilki02@gmail.com>
2018-11-12 00:46:31 -0500

    Sets styles and starts javascript

--- final.js
+++ final.js
@@ -1,17 +1,56 @@
-$(document).ready(function() {
 
-
+function generateComputerNum() {
+  var computer = [1,2,3];
+  var random = computer[Math.floor(Math.random()*computer.length)];
+    if (random === 1){
+    computer = "Rock!";
+    }else if (random === 2){
+    computer = "Paper!";
+    }else {
+    computer = "Scissor!";
+    }
+    document.getElementById('computer-display').value = computer;
 }
 
 
+function rockSelect() {
+    document.getElementById('user-display').value = "Rock!";
+    generateComputerNum();
+}
+function paperSelect() {
+    document.getElementById('user-display').value = "Paper!";
+    generateComputerNum();
+}
+function scissorsSelect() {
+    document.getElementById('user-display').value = "Scissors!";
+    generateComputerNum();
+}
 
-//Set buttons to a value
+document.getElementById('rock-selector').onclick=rockSelect;
+document.getElementById('paper-selector').onclick=paperSelect;
+document.getElementById('scissors-selector').onclick=scissorsSelect;
+
+// document.getElementById('rock-selector').onclick = userPlaySelect;
+// function computerNumSet() {
+// }
+
+// }
+
+
+
+
+
+
+// });
+
+
+
+//Set buttons to a value  --- done
 //
 //Set game rules
 //
-//Random Generator
+//Random Generator --- done 
 //
-//If wins = 5, show next page link
 //
 //Track wins, losses and ties
 //
