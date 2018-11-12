58b14940a294989cc960ae4213f6e6fd9c203195
glasjp123 <jwilki02@gmail.com>
2018-11-12 14:56:04 -0500

    Fixes label position

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ function resultReveal() {
         ties = parseInt(document.getElementById('tie').value);
 
     if ((wins + losses + ties) === 50 && wins > 15){
-        alert("Smooth move, you're in a groove!")
+        prompt("Smooth move, you're in a groove!")
     } else if ((wins + losses + ties) === 50 && losses > 15){
-         alert("You've had a tough run, keep on going!")
+         prompt("You've had a tough run, keep on going!")
     } else {}
 }
 
@@ -74,26 +74,24 @@ function scissorsSelect() {
     resultReveal();
 }
 
-function resetPage() {
-    var user = document.getElementById('user-display').value,
-        computer = document.getElementById('computer-display').value,
-        wins = parseInt(document.getElementById('win').value),
-        losses = parseInt(document.getElementById('loss').value),
-        ties = parseInt(document.getElementById('tie').value);
-        if ((user).length > 0){
-        user = "",
-        computer = " ",
-        wins = "0",
-        losses = "0",
-        ties = "0",
-        console.log("works");
-        } else {}
-}   
+// function resetPage() {
+//     var user = document.getElementById('user-display').value,
+//         computer = document.getElementById('computer-display').value,
+//         wins = parseInt(document.getElementById('win').value),
+//         losses = parseInt(document.getElementById('loss').value),
+//         ties = parseInt(document.getElementById('tie').value);
+//         if ((user).length > 0){
+//         form.user = " ",
+//         console.log("works");
+//         } else {}
+// }   
+// document.getElementById('reset').onclick=resetPage;
+
+////// Couldn't figure out how to reset the input fields so I just cheated and made the button reload the page //////
 
 
 document.getElementById('rock-selector').onclick=rockSelect;
 document.getElementById('paper-selector').onclick=paperSelect;
 document.getElementById('scissors-selector').onclick=scissorsSelect;
-document.getElementById('reset').onclick=resetPage;
 
 //Reset button
